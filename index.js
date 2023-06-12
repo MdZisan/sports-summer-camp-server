1e137be7a2bc08b70f98c6eb6b733119cced0d55
MD Zisan <zisanislam01@gmail.com>
2023-06-12 21:22:24 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =  require('express');
 const app =  express();
 const cors = require('cors');
-// const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const port =  process.env.PORT || 5000;
 require('dotenv').config()
 const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
@@ -9,6 +9,23 @@ const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
 app.use(cors());
 app.use(express.json())
 
+//jwt
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 // MongoDB
 
@@ -34,11 +51,17 @@ async function run() {
     const selectedClassesCollection = client.db('summer_camp').collection('selectedClasses')
     const paymentHistoryCollection = client.db('summer_camp').collection('paymentHistory')
 
+//jwt 
+app.post('/jwt', (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 
+  res.send({ token })
+})
 // users 
 app.post('/users',async(req,res)=>{
   const user =  req.body;
-  console.log(user);
+  // console.log(user);
   const existing = await usersCollection.findOne({email:user.email})
    
       if(existing){
@@ -64,7 +87,7 @@ res.send(result)
 
 })
 
-app.get('/users',async(req,res)=>{
+app.get('/users',verifyJWT,async(req,res)=>{
   let filter = {};
   if(req.query.email){
   filter = {email: req.query.email};
@@ -94,6 +117,7 @@ app.post('/selectedClass',async(req,res)=>{
   delete selectedClasses._id;
   delete selectedClasses.status;
   delete selectedClasses.feedback;
+  delete selectedClasses.studentEnrolled;
   selectedClasses.classId = classId;
   selectedClasses.classStatus= 'selected'
 
@@ -125,11 +149,35 @@ app.get('/selectedClass',async(req,res)=>{
 //classes
 app.post('/classes',async(req,res)=>{
  const classes = req.body;
+
+ classes.studentEnrolled = 0;
+
 // console.log(classes);
  const result= await classesCollection.insertOne(classes)
  res.send(result)
 
 
+})
+
+/// classes seats increases by instructor
+app.patch('/updateClasses/:id',async(req,res)=>{
+  const id= req.params.id;
+  const  {seats}= req.body;
+const filter = {_id: new ObjectId(id)};
+const updateDoc = {
+  $inc:{
+    availableSeats: seats,
+  }
+}
+const result =  await classesCollection.updateOne(filter,updateDoc)
+res.send(result)
+  // console.log(id, seats);
+
+})
+app.get('/updateClasses/:id',async(req,res)=>{
+  const id = req.params.id;
+  const result = await classesCollection.find({_id: new ObjectId(id)}).toArray();
+  res.send(result)
 })
 
 //class status update
@@ -177,6 +225,17 @@ app.get('/classes',async(req,res)=>{
   res.send(result)
 
 })
+
+app.get('/popularClasses',async(req,res)=>{
+  
+  let filter = {};
+ 
+  if(req.query.status){
+    filter = {status: req.query.status};
+  }
+  const result = await classesCollection.find(filter).sort({studentEnrolled:-1}).limit(6).toArray()
+  res.send(result)
+})
 //payment
 app.post('/payment',async(req,res)=>{
  
@@ -202,7 +261,8 @@ app.post('/payment',async(req,res)=>{
   const allClassesUpdateDoc = {
    
     $inc: {
-      availableSeats: -1
+      availableSeats: -1,
+      studentEnrolled: 1
     }
   };
 
@@ -231,7 +291,11 @@ app.post('/create-payment-intent', async (req, res) => {
   })
 })
 
-
+app.get('/paymentHistory',async(req,res)=>{
+  const {email} = req.query;
+  const result = await paymentHistoryCollection.find({email: email}).sort({date:-1}).toArray();
+  res.send(result)
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
